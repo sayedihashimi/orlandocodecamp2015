d4bc429558547ba85310832ae57c36692b9b571d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2015-03-28 02:41:03 -0400

    updating samples

---
+++ PublishSamples/src/e2e-azure-optimize-images.ps1
@@ -0,0 +1,51 @@
+﻿[cmdletbinding()]
+param(
+    [string]$sourceRoot,
+    [string]$pubTempDir,
+    [string]$packOutDir,
+    [string]$publishPwd = $env:AzureSayedPublishPwd
+)
+
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+$scriptDir = ((Get-ScriptDirectory) + "\")
+
+if(!$sourceRoot){ $sourceRoot = $scriptDir }
+if(!$pubTempDir){ $pubTempDir = (Join-Path $env:temp 'sayed\e2e\publish\pubtemp') }
+if(!$packOutDir){ $packOutDir = (Join-Path $pubTempDir 'packout') }
+if(!$publishPwd){ throw ('Publish password missing') }
+if(!(Test-Path $sourceRoot)){ throw ('Source root not found at: [{0}]' -f $sourceRoot) }
+
+if(Test-Path $pubTempDir){ Remove-Item $pubTempDir -Recurse -Force }
+if(Test-Path $packOutDir){ Remove-Item $packOutDir -Recurse -Force }
+
+New-Item $pubTempDir -ItemType Directory
+New-Item $packOutDir -ItemType Directory
+
+$packOutDir = (Get-Item $packOutDir).FullName
+
+try{
+    Push-Location
+    $srcDir = (get-item (join-path $sourceRoot 'WebApp01')).FullName
+    Set-Location $srcDir
+
+    kpm restore
+    kpm build
+
+    kpm @('bundle', '-o', "$packOutDir")
+    Set-Location $packOutDir
+}
+finally{
+    Pop-Location
+}
+
+'Publishing to Azure' | Write-Output
+$pubscript = (get-item (Join-Path $sourceRoot 'PubSamples\Properties\PublishProfiles\OptimizeImages-publish.ps1')).FullName
+&($pubscript) -packOutput $packOutDir -publishProperties @{
+     'WebPublishMethod'='MSDeploy'
+     'MSDeployServiceURL'='sayed01.scm.azurewebsites.net:443';
+     'DeployIisAppPath'='sayed01';'Username'='$sayed01';'Password'="$publishPwd"
+}
